a7c49fc1c0868063dd43dac8cccedc33463037ab
Leggy <50837811+legacy-sh@users.noreply.github.com>
2020-12-12 12:14:15 -0500

    Update All_ADUsers.ps1

--- Active_Directory/All_ADUsers.ps1
+++ Active_Directory/All_ADUsers.ps1
@@ -1,6 +1,6 @@
 # List of all AD Users
 
-Install-Module -Name Active Directory
+Install-Module -Name ActiveDirectory
 Import-Module ActiveDirectory
 
 Get-ADUser -Filter * -Properties Enabled,Name,DistinguishedName | ~\Desktop\ADUser.txt
