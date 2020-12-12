61ba4b31bf9ab660d367d818a0945d96765db550
Leggy <50837811+legacy-sh@users.noreply.github.com>
2020-12-12 12:02:13 -0500

    Rename lastlogon-w-export.ps1 to Lastlogon_W_Export.ps1

--- Active_Directory/lastlogon-w-export.ps1
+++
@@ -1,4 +0,0 @@
-Install-Module -Name ActiveDirectory
-Import-Module ActiveDirectory
-
-Get-ADUser -Filter {Enabled -eq $true} -Properties Name,Manager,LastLogon | Select-Object Name,Manager,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}} | Start-Transcript -path ~\Desktop\export.txt -Append | Stop-Transcript
